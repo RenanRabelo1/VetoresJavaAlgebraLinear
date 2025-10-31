bd1103edaa5fb58b4a073a9e433f661b1fe9d568
RenanRabelo1 <renanrabelo82@gmail.com>
2025-10-31 07:27:01 -0300

    Fim

--- src/Transformations.java
+++ src/Transformations.java
@@ -1,24 +1,175 @@
 public class Transformations {
-        public double[][] Rotation2D(double[][]vector, double angle) {
-            LinearAlgebra linearAlgebra = new LinearAlgebra();
-           double [][] vector2 = {
-                    {vector[0][0]},
-                    {vector[1][0]},
-                    {1}
-            };
-            double[][] rotationMatrix = {
-                    {Math.cos(angle), -Math.sin(angle), 0},
-                    {Math.sin(angle), Math.cos(angle), 0},
-                    {0, 0, 1}
-            };
-            double [][] result = linearAlgebra.dot(rotationMatrix, vector2);
+    // ========== DOUBLE[] VECTOR METHODS ==========
 
-            double[][] vector3 = {
-                    {result[0][0]},
-                    {result[1][0]}
-            };
-            return vector3;
-        }
+    public double[] translate2D(double[] vector, double dx, double dy) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {1}
+        };
+
+        double[][] T = {
+                {1, 0, dx},
+                {0, 1, dy},
+                {0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(T, v);
+
+        double[] novo = {resultado[0][0], resultado[1][0]};
+        return novo;
+    }
+
+    public double[] translate3D(double[] vector, double dx, double dy, double dz) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {vector[2]},
+                {1}
+        };
+
+        double[][] T = {
+                {1, 0, 0, dx},
+                {0, 1, 0, dy},
+                {0, 0, 1, dz},
+                {0, 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(T, v);
+
+        double[] novo = {resultado[0][0], resultado[1][0], resultado[2][0]};
+        return novo;
+    }
+
+
+    public double[] reflection2DX(double[] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {1}
+        };
+
+        double[][] R = {
+                {-1, 0, 0},
+                { 0, 1, 0},
+                { 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(R, v);
+
+        double[] novo = {resultado[0][0], resultado[1][0]};
+        return novo;
+    }
+
+    public double[] reflection2DY(double[] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {1}
+        };
+
+        double[][] R = {
+                { 1, 0, 0},
+                { 0,-1, 0},
+                { 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(R, v);
+        double[] novo = {resultado[0][0], resultado[1][0]};
+        return novo;
+    }
+
+    public double[] reflection3DX(double[] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {vector[2]},
+                {1}
+        };
+
+        double[][] R = {
+                {-1, 0, 0, 0},
+                { 0, 1, 0, 0},
+                { 0, 0, 1, 0},
+                { 0, 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(R, v);
+        double[] novo = {resultado[0][0], resultado[1][0], resultado[2][0]};
+        return novo;
+    }
+
+    public double[] reflection3DY(double[] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {vector[2]},
+                {1}
+        };
+
+        double[][] R = {
+                { 1, 0, 0, 0},
+                { 0,-1, 0, 0},
+                { 0, 0, 1, 0},
+                { 0, 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(R, v);
+        double[] novo = {resultado[0][0], resultado[1][0], resultado[2][0]};
+        return novo;
+    }
+
+    public double[] reflection3DZ(double[] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double[][] v = {
+                {vector[0]},
+                {vector[1]},
+                {vector[2]},
+                {1}
+        };
+
+        double[][] R = {
+                { 1, 0, 0, 0},
+                { 0, 1, 0, 0},
+                { 0, 0,-1, 0},
+                { 0, 0, 0, 1}
+        };
+
+        double[][] resultado = linearAlgebra.dot(R, v);
+        double[] novo = {resultado[0][0], resultado[1][0], resultado[2][0]};
+        return novo;
+    }
+
+    // ========== DOUBLE[][] VECTOR METHODS ==========
+
+    // Rotations
+    public double[][] Rotation2D(double[][]vector, double angle) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {1}
+        };
+        double[][] rotationMatrix = {
+                {Math.cos(angle), -Math.sin(angle), 0},
+                {Math.sin(angle), Math.cos(angle), 0},
+                {0, 0, 1}
+        };
+        double [][] result = linearAlgebra.dot(rotationMatrix, vector2);
+
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]}
+        };
+        return vector3;
+    }
+
     public double[][] Rotation3DX(double[][] vector, double angle) {
         LinearAlgebra linearAlgebra = new LinearAlgebra();
 
@@ -38,9 +189,7 @@ public class Transformations {
                 {0, 0, 0, 1}
         };
 
-
         double[][] result = linearAlgebra.dot(rotationMatrix, vector2);
-
 
         double[][] vector3 = {
                 {result[0][0]},
@@ -50,9 +199,9 @@ public class Transformations {
 
         return vector3;
     }
+
     public double[][] Rotation3DY(double[][] vector, double angle) {
         LinearAlgebra linearAlgebra = new LinearAlgebra();
-
 
         double[][] vector2 = {
                 {vector[0][0]},
@@ -60,7 +209,6 @@ public class Transformations {
                 {vector[2][0]},
                 {1}
         };
-
 
         double[][] rotationMatrix = {
                 {Math.cos(angle), 0, Math.sin(angle), 0},
@@ -82,14 +230,12 @@ public class Transformations {
     public double[][] Rotation3DZ(double[][] vector, double angle) {
         LinearAlgebra linearAlgebra = new LinearAlgebra();
 
-
         double[][] vector2 = {
                 {vector[0][0]},
                 {vector[1][0]},
                 {vector[2][0]},
                 {1}
         };
-
 
         double[][] rotationMatrix = {
                 {Math.cos(angle), -Math.sin(angle), 0, 0},
@@ -107,6 +253,138 @@ public class Transformations {
         };
         return vector3;
     }
+
+    // Projections
+    public double[][] Projection2DX(double[][] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+
+        //Recebe o vector
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {1}
+        };
+
+        //Multiplica para uma matriz 3x3 no qual [x, y, 1] vira [x, 0, 1]
+        double[][] projectionMatrix = {
+                {1, 0, 0},
+                {0, 0, 0},
+                {0, 0, 1}
+        };
+
+        double[][]result = linearAlgebra.dot(projectionMatrix, vector2);
+        //Retorna o resultado
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]},
+        };
+        return vector3;
+    }
+
+    public double [][] Projection2DY(double[][] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {1}
+        };
+
+        //Multiplica para uma matriz 3x3 no qual [x, y, 1] vira [0, y, 1]
+        double[][] projectionMatrix = {
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 1}
+        };
+
+        double[][]result = linearAlgebra.dot(projectionMatrix, vector2);
+
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]},
+        };
+
+        return vector3;
+    }
+
+    public double [][]  Projection3DX(double[][] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {vector[2][0]},
+                {1}
+        };
+
+        double[][] projectionMatrix = {
+                {1, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 1}
+        };
+
+        double[][]result = linearAlgebra.dot(projectionMatrix, vector2);
+
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]},
+                {result[2][0]},
+        };
+        return vector3;
+    }
+
+    public double [][]  Projection3DY(double[][] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {vector[2][0]},
+                {1}
+        };
+
+        double[][] projectionMatrix = {
+                {0, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 1}
+        };
+
+        double[][]result = linearAlgebra.dot(projectionMatrix, vector2);
+
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]},
+                {result[2][0]},
+        };
+        return vector3;
+    }
+
+    public double [][]  Projection3DZ(double[][] vector) {
+        LinearAlgebra linearAlgebra = new LinearAlgebra();
+        double [][] vector2 = {
+                {vector[0][0]},
+                {vector[1][0]},
+                {vector[2][0]},
+                {1}
+        };
+
+        double[][] projectionMatrix = {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+
+        double[][]result = linearAlgebra.dot(projectionMatrix, vector2);
+
+        double[][] vector3 = {
+                {result[0][0]},
+                {result[1][0]},
+                {result[2][0]},
+        };
+        return vector3;
+    }
+
     public double[][] Shearing(double[][] vector, double kx, double ky) {
         LinearAlgebra linearAlgebra = new LinearAlgebra();
 
@@ -136,7 +414,3 @@ public class Transformations {
         return vector3;
     }
 }
-
-
-
-
